60d1c45813aa5392b35f11d595f827dce96dad71
Juan <juanalbertoh.fernandez13@gmail.com>
2026-01-26 14:29:01 +0100

    docs(Match): cambia la referencia participante a jugador.

--- src/main/kotlin/models/Match.kt
+++ src/main/kotlin/models/Match.kt
@@ -2,7 +2,7 @@ package com.github.juanalberticohf.models
 
 /**
  * La clase Match representa todos los datos de una partida.
- * @param players Lista de participantes en la partida.
+ * @param players Lista de jugadores en la partida.
  * @param isMatchEnded Indica si la partida ha finalizado.
  * @param rounds Lista de rondas jugadas en la partida.
  */
