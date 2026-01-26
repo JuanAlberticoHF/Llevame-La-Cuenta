ba2ae93411ed25b3b2a5657b56793ba4006880f6
Juan <juanalbertoh.fernandez13@gmail.com>
2026-01-26 14:37:32 +0100

    feat(RoundController): crea el controlador pendiente de implementar.

---
+++ src/main/kotlin/controllers/RoundController.kt
@@ -0,0 +1,9 @@
+package com.github.juanalberticohf.controllers
+
+/**
+ * Controlador de ronda
+ * Gestiona el flujo de una ronda dentro de una partida
+ */
+class RoundController {
+    // TODO: Implementar controlador de ronda
+}
