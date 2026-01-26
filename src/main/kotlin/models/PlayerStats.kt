5e63c756ef6a29785af8a4ff8bc792ed8dc7c6cd
Juan <juanalbertoh.fernandez13@gmail.com>
2026-01-26 14:36:43 +0100

    feat(PlayerStats): crea y documenta el modelo para representar las estadisticas de un jugador.

---
+++ src/main/kotlin/models/PlayerStats.kt
@@ -0,0 +1,14 @@
+package com.github.juanalberticohf.models
+
+/**
+ * La clase PlayerStats representa las estadísticas de un jugador en una ronda.
+ * @param playerId Identificador del jugador.
+ * @param payAmount Cantidad a pagar en la ronda (si aplica).
+ */
+data class PlayerStats(
+    val playerId: Int,
+    val payAmount: Int,
+//    val status: PlayerState, // Estado del jugador al finalizar la ronda.
+//    val bathroomCount: Int, // Número de veces que el jugador ha ido al baño.
+//    val tips: Int, // Propinas dadas por el jugador después de pedir la cuenta.
+)
