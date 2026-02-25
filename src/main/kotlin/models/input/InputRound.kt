f8528d754963c5937e0eeefbd7fa255c5ae08ef5
Juan <juanalbertoh.fernandez13@gmail.com>
2026-02-25 16:55:50 +0100

    feat(InputRound.kt): crea una clase para parsear el input del usuario en bruto a un objeto Round.

---
+++ src/main/kotlin/models/input/InputRound.kt
@@ -0,0 +1,47 @@
+package com.github.juanalberticohf.models.input
+
+import com.github.juanalberticohf.models.PlayerStats
+import com.github.juanalberticohf.models.enums.PaymentMethod
+import com.github.juanalberticohf.models.Round
+
+data class InputRound (
+    val roundNumber: Int,
+) {
+    var billRequester: Int = -1
+        set(value) {
+            if (value in 1..8)
+                field = value
+            else
+                throw IllegalArgumentException("El valor de billRequester debe estar entre 1 y 8")
+        }
+    var billAmount: Int = -1
+        set(value) {
+            if (value >= 0)
+                field = value
+            else
+                throw IllegalArgumentException("El valor de billAmount no puede ser negativo")
+        }
+    var paymentMethod: PaymentMethod = PaymentMethod.SINGLE_PAYER
+    var hasSufficientCards: Boolean = false
+    val playersStats: MutableList<PlayerStats> = mutableListOf()
+
+    // FUNCIONES //
+    fun procesarRonda() : Round {
+        return Round(
+            roundNumber,
+            billRequester,
+            billAmount,
+            paymentMethod,
+            hasSufficientCards,
+            playersStats
+        )
+    }
+}
+
+//    var lowestCardValue: Int = 0
+//        set(value) {
+//            if (value >= 0)
+//                field = value
+//            else
+//                throw IllegalArgumentException("El valor de lowestCardValue no puede ser negativo")
+//        }
