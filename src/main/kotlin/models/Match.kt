604cb5c0924a2cf94e91c30be2c40d75bfb19b37
Juan <juanalbertoh.fernandez13@gmail.com>
2026-02-25 16:50:04 +0100

    refactor(Match.kt): cambia el tipo de lista de List<T> a MutableList<T>

--- src/main/kotlin/models/Match.kt
+++ src/main/kotlin/models/Match.kt
@@ -8,6 +8,6 @@ package com.github.juanalberticohf.models
  */
 data class Match(
     val players: List<Player>,
-    val rounds : List<Round> = mutableListOf(),
+    val rounds : MutableList<Round> = mutableListOf(),
     val isMatchEnded: Boolean = false
 )
