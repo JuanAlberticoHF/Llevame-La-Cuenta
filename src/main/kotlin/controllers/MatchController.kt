f898b4113d6d3297f9581ca4d20c4088b070f90b
Juan <juanalbertoh.fernandez13@gmail.com>
2026-02-26 19:19:01 +0100

    feat(MatchController.kt): agrega las funciones restantes al controlador para la gestion de la ronda y final de partida.

--- src/main/kotlin/controllers/MatchController.kt
+++ src/main/kotlin/controllers/MatchController.kt
@@ -7,8 +7,14 @@ import com.github.juanalberticohf.views.MatchView
 
 /**
  * Controlador de la partida.
- * Gestiona el flujo de la partida, desde la obtención de datos iniciales
- * hasta la creación de la instancia de Match.
+ * Gestiona el flujo de la partida, desde la obtención de datos iniciales hasta la finalización de la partida mostrando el resultado final y el ganador.
+ * Utiliza la vista de la partida para interactuar con el usuario y mostrar la información relevante durante la partida.
+ * Crea una instancia de Match con los jugadores y un controlador de ronda para gestionar las rondas de la partida.
+ * El bucle principal de la partida se ejecuta mientras la partida no haya finalizado, iniciando nuevas rondas.
+ * Una vez finalizada la partida se llama al mét0do matchEnd para mostrar el resultado final y el ganador.
+ * @see MatchView
+ * @see Match
+ * @see Player
  */
 class MatchController {
     val matchView = MatchView() // Instancia de la vista de la partida
@@ -31,7 +37,59 @@ class MatchController {
         matchView.showRules() // Mostrar reglas basicas del juego
         matchView.requestStartMatch() // Solicitar iniciar la partida
 
+        matchInProgress(playerList) // Iniciar la partida
+    }
+
+    /**
+     * Gestiona el flujo principal de la partida, creando una instancia de Match con los jugadores
+     * y un controlador de ronda para gestionar las rondas de la partida.
+     * El bucle principal de la partida se ejecuta mientras la partida no haya finalizado, iniciando nuevas rondas.
+     * Una vez finalizada la partida, se llama al mét0do matchEnd para mostrar el resultado final y el ganador.
+     * @param playerList Lista de objetos Player que participan en la partida
+     */
+    fun matchInProgress(playerList: List<Player>) {
         val match = Match(playerList) // Creación de la partida con los jugadores
+
+        val roundController = RoundController(match) // Creación del controlador de ronda con la partida
+
+        // Bucle principal de la partida, se ejecuta mientras la partida no haya finalizado
+        while (!match.isMatchEnded) {
+            roundController.newRound() // Iniciar la primera o siguiente ronda
+        }
+
+        matchEnd(match) // Finalizar la partida
+    }
+
+    /**
+     * Gestiona el flujo de finalización de la partida, mostrando el resultado final y el ganador.
+     * Busca el jugador sin ahorros para mostrar un mensaje de fin de partida,
+     * calcula el jugador ganador o solicita el desempate en caso y finalizar la partida finalmente.
+     * @param match Instancia de Match con el estado final de la partida
+     */
+    fun matchEnd(match: Match) {
+        // Muestra un mensaje indicando el fin de la partida
+        val playerWithoutSaves = match.players.find { it.currentSaves <= 0 }!! // Buscar un jugador sin ahorros
+        matchView.showEndOfMatch(playerWithoutSaves)
+        // Solicitar al usuario que pulse ENTER para finalizar la partida
+        matchView.requestEndOfMatch()
+        // Calcular el jugador ganador o solicitar el desempate en caso de empate
+        val maxSavings = match.players.maxOf { it.currentSaves } // Ahorros maximos entre los jugadores
+        val winners: List<Player> = match.players.filter { it.currentSaves == maxSavings } // Lista de jugadores con los ahorros maximos (puede haber empates)
+
+        if (winners.size == 1) { // Si hay un solo ganador, mostrar su nombre y ahorros finales
+            val winner = winners.first()
+            winner.isWinner = true
+            matchView.showWinner(winner)
+        } else if (winners.size > 1) {// Si hay varios ganadores se solicita el desempate.
+            val winner = matchView.showAndRequestWinner(winners)
+            winner.isWinner = true
+            matchView.showWinner(winner)
+        } else {
+            throw Exception("Error al calcular el ganador de la partida.")
+        }
+
+        // Solicitar al usuario que pulse ENTER volver al menu principal
+        matchView.requestGoToMenu()
     }
 
     /**
@@ -45,7 +103,7 @@ class MatchController {
 
         for (i in playerNameList.indices) {
             val player = Player (
-                i,
+                i+1,
                 playerNameList[i],
                 startingMoney
             )
