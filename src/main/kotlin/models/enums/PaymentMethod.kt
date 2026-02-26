83baac03689ae914cbfbc92cd1793f17f4038028
Juan <juanalbertoh.fernandez13@gmail.com>
2026-02-26 18:46:24 +0100

    refactor(PaymentMethod.kt): elimina valueOrString y agrega sobreescribiendo el metodo toString()

--- src/main/kotlin/models/enums/PaymentMethod.kt
+++ src/main/kotlin/models/enums/PaymentMethod.kt
@@ -7,17 +7,11 @@ package com.github.juanalberticohf.models.enums
 enum class PaymentMethod {
     SINGLE_PAYER, FIFTY_FIFTY, GO_DUTCH;
 
-    /**
-     * Permite introducir un String para obtener un PaymentMethod
-     * @param value metodo de pago en String
-     * @return devuelve el PaymentMethod correspondiente
-     */
-    fun valueOfString (value: String): PaymentMethod {
-        return when (value.uppercase()) {
-            "SINGLE_PAYER" -> SINGLE_PAYER
-            "FIFTY_FIFTY" -> FIFTY_FIFTY
-            "GO_DUTCH" -> GO_DUTCH
-            else -> throw IllegalArgumentException("El valor de paymentMethod no es válido")
+    override fun toString(): String {
+        return when (this) {
+            SINGLE_PAYER -> "UN PAGADOR"
+            FIFTY_FIFTY -> "A MEDIAS"
+            GO_DUTCH -> "A PACHAS"
         }
     }
 }
