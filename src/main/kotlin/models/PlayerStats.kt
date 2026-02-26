7175b28564e3090f79d026be36cd75d6b631dbb3
Juan <juanalbertoh.fernandez13@gmail.com>
2026-02-26 18:48:27 +0100

    feat(PlayerStats.kt): agrega un nuevo atributo a la clase para la cuenta de fichas obtenidas.

--- src/main/kotlin/models/PlayerStats.kt
+++ src/main/kotlin/models/PlayerStats.kt
@@ -4,10 +4,12 @@ package com.github.juanalberticohf.models
  * La clase PlayerStats representa las estadísticas de un jugador en una ronda.
  * @param playerId Identificador del jugador.
  * @param payAmount Cantidad a pagar en la ronda (si aplica).
+ * @param obtainedTokens Numero de fichas obtenidas por el jugador en la ronda (si aplica).
  */
 data class PlayerStats(
     val playerId: Int,
-    val payAmount: Int,
+    val payAmount: Double,
+    val obtainedTokens: Int,
 //    val status: PlayerState, // Estado del jugador al finalizar la ronda.
 //    val bathroomCount: Int, // Número de veces que el jugador ha ido al baño.
 //    val tips: Int, // Propinas dadas por el jugador después de pedir la cuenta.
