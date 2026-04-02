04beb61bcc0b9b86ebe66658c8ffcb605a5d6793
Juan <juanalbertoh.fernandez13@gmail.com>
2026-04-02 17:53:59 +0200

    test: crea el test para el controlador RoundController sin implementar todos los tests.

---
+++ src/test/kotlin/controllers/RoundControllerTest.kt
@@ -0,0 +1,16 @@
+package controllers
+
+import org.junit.jupiter.api.Assertions.*
+import kotlin.test.Test
+
+class RoundControllerTest {
+    @Test
+    fun testNewRound(){
+        // TODO: Implementar
+    }
+
+    @Test
+    fun testGetPaymentMethod(){
+        // TODO: Implementar
+    }
+}
