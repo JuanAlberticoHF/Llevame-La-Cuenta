377469ed635e79192ea4fcb805645f40982fb9c5
Juan <juanalbertoh.fernandez13@gmail.com>
2026-04-02 17:42:27 +0200

    refactor(): modifica el constructor de MainController, modifica la logica del metodo startApp creando un manejador de opciones.
    MainController ahora permite inyección de dependencias e inicializa matchController en el constructor

--- src/main/kotlin/controllers/MainController.kt
+++ src/main/kotlin/controllers/MainController.kt
@@ -1,13 +1,16 @@
 package com.github.juanalberticohf.controllers
 
 import com.github.juanalberticohf.views.AppView
+import com.github.juanalberticohf.views.IAppView
 
 /**
  * Controlador principal de la aplicacion
  * Gestiona el flujo principal del programa
  */
-class MainController {
-    val appView = AppView()
+class MainController (
+    val appView: IAppView = AppView(),
+    private val matchController: IMatchController = MatchController()
+) {
 
     /**
      * Inicia la aplicación mostrando un mensaje de bienvenida y el menu principal.
@@ -16,24 +19,28 @@ class MainController {
     fun startApp() {
         appView.welcomeMessage() // Vista de bienvenida
 
-        var looping: Boolean = true
-        while(looping) {
-            val userOption = appView.mainMenu() // Mostrar vista menu principal y obtener opcion
+        while(true) {
+            // Mostrar vista menu principal y obtener opcion
+            val option = appView.mainMenu()
 
-            when(userOption) {
-                1 -> { // Crea el controlador de la partida y comienza una nueva partida
-                    val matchController = MatchController()
-                    matchController.matchStart()
-                }
-                0 -> { // Salir del programa
-                    looping = false
-                    appView.leaveMessage()
-                }
-                else -> {
-                    appView.notAvaiableMessage(userOption)
-                }
-            }
+            handleOption(option)
+
+            if (option == 0) break
         }
 
+    }
+
+    /**
+     * Gestiona las opciones seleccionadas por el usuario en el menu principal.
+     * @param option Opcion seleccionada por el usuario.
+     */
+    private fun handleOption(option: Int) {
+        when(option) {
+            1 -> { matchController.matchStart() } // Crea el controlador de la partida y comienza una nueva partida
+            0 -> {
+                appView.leaveMessage()
+            } // Salir del programa
+            else -> { appView.notAvaiableMessage(option) }
+        }
     }
 }
